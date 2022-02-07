80d262a9f87bdbc3d0d93c5ae4078eb4eb83391b
Mustafa Tayyip Yetiş <tayyipyetis@outlook.com>
2022-02-07 22:27:16 +0300

    changed app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Layout from "./layout/Layout";
+import Home from "./views/Home";
+import About from "./views/About";
+import Posts from "./views/Posts";
+import PostDetail from "./views/PostDetail";
+import Login from "./views/Login";
+import PrivateRoute from "./utils/PrivateRoute";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login" exact component={Login} />
+        <Layout>
+          <PrivateRoute path="/" exact component={Home} />
+          <PrivateRoute path="/about" component={About} />
+          <PrivateRoute path="/posts" exact component={Posts} />
+          <PrivateRoute path="/posts/:id" component={PostDetail} />
+        </Layout>
+      </Switch>
+    </BrowserRouter>
   );
 }
-
-export default App;
